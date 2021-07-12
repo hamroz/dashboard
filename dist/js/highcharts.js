448f44aea0801ced9d82e2fc20e1abc38ca23739
hamroz <hamroz.gavharov@gmail.com>
2021-07-12 15:26:57 +0500

    minor changes

--- dist/js/highcharts.js
+++ dist/js/highcharts.js
@@ -234,7 +234,6 @@ $(document).ready(function () {
   $("#tableasses").DataTable();
 });
 
-
 // PIN - SIDEBAR
 
 let container = document.querySelector(".content");
@@ -247,4 +246,82 @@ pinBtn.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
   container.classList.remove("sticky-sidebar-right");
+});
+
+// PIE - CHART
+
+Highcharts.chart("piechart", {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: "pie",
+  },
+  title: {
+    text: "Browser market shares in January, 2018",
+  },
+  tooltip: {
+    pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+  },
+  accessibility: {
+    point: {
+      valueSuffix: "%",
+    },
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: "pointer",
+      dataLabels: {
+        enabled: true,
+        format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+      },
+    },
+  },
+  series: [
+    {
+      name: "Brands",
+      colorByPoint: true,
+      data: [
+        {
+          name: "Chrome",
+          y: 61.41,
+          sliced: true,
+          selected: true,
+        },
+        {
+          name: "Internet Explorer",
+          y: 11.84,
+        },
+        {
+          name: "Firefox",
+          y: 10.85,
+        },
+        {
+          name: "Edge",
+          y: 4.67,
+        },
+        {
+          name: "Safari",
+          y: 4.18,
+        },
+        {
+          name: "Sogou Explorer",
+          y: 1.64,
+        },
+        {
+          name: "Opera",
+          y: 1.6,
+        },
+        {
+          name: "QQ",
+          y: 1.2,
+        },
+        {
+          name: "Other",
+          y: 2.61,
+        },
+      ],
+    },
+  ],
 });
